3f5eaf0bb25c94984b08e4b8b6c0b996792e1bf1
Chrysovalantis Moustakakis <c.moustakakis@alumnos.upm.es>
2023-06-22 16:29:44 +0200

    schema update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 drop table if exists demo_app_user;
 drop table if exists demo_user_reports;
+drop table if exists demo_street_info;
+drop table if exists demo_street_measurements;
 
 create table demo_app_user (
 	user_id bigint not null auto_increment,
@@ -27,15 +29,17 @@ create table demo_street_info (
 	severity varchar(255) not null,
 	latitude float(13,10) not null,
 	longitude float(13,10) not null,
-    constraint unique_latitude_longitude unique (latitude, longitude)
+	constraint unique_latitude_longitude unique (latitude, longitude)
 );
 
 create table demo_street_measurements (
 	measurement_id bigint primary key auto_increment,
 	ts bigint not null,
+	user_id varchar(255) not null,
 	longitude float(12,10) not null,
 	latitude float(12,10) not null,
 	accelerometer_x float(12,10) not null,
 	accelerometer_y float(12,10) not null,
 	accelerometer_z float(12,10) not null
 );
+
